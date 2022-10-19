1d4b5b302f9808a14c6321e0c1ae7111396dd97a
Aidan Matchette <frigasack@gmail.com>
2022-10-18 22:37:26 -0700

    Update tests.py

--- pizza_app/tests.py
+++ pizza_app/tests.py
@@ -6,17 +6,17 @@ client = Client()
 
 class PizzaTestCase(TestCase):
     def setUp(self):
-        return Pizza.objects.create(pizza_name="Pepperoni")
+        return Pizza.objects.create(pizza_name="Test Pizza")
 
     def test_pizza_creation(self):
-        pepperoni_pizza = self.setUp()
+        test_pizza = self.setUp()
 
-        self.assertTrue(isinstance(pepperoni_pizza, Pizza))
+        self.assertTrue(isinstance(test_pizza, Pizza))
 
 
 class IngredientsTestCase(TestCase):
     def setUp(self):
-        return Ingredients.objects.create(ingredient_name="onion")
+        return Ingredients.objects.create(ingredient_name="test")
 
     def test_ingredient_creation(self):
         new_ingredient = self.setUp()
